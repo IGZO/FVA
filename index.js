89c7737a625e4f65df537ceb75b8116b22a746d3
ABDELLAH JAIZE <crazy.igzo@gmail.com>
2020-08-17 16:10:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = express();
 
 app.use(express.static('dist',{extensions:['html']}));
 
-app.listen(process.env.PORT || 3000, () => console.log('fva app listening on port '+ process.env.PORT || 3000));
+app.listen(3000, () => console.log('fva app listening on port '+ 3000));
+//app.listen(process.env.PORT || 3000, () => console.log('fva app listening on port '+ process.env.PORT || 3000));
